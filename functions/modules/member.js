c9d12f2cbef6716e344ca247c62771a7d1a1970e
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-04-19 11:49:10 +0900

    commit

--- functions/modules/member.js
+++ functions/modules/member.js
@@ -1,9 +1,21 @@
-const { onDocumentDeleted } = require('firebase-functions/v2/firestore')
+const {
+  onDocumentDeleted,
+  onDocumentCreated,
+} = require('firebase-functions/v2/firestore')
 const { getFirestore } = require('firebase-admin/firestore')
 const { getStorage } = require('firebase-admin/storage')
 const { logger } = require('firebase-functions/v2')
 const firestore = getFirestore()
 const storage = getStorage()
+
+/**
+ * ### exports.createMember
+ * Process when a Member document is created.
+ * - Synchronize number of members to the system document.
+ */
+exports.createMember = onDocumentCreated('Members/{docId}', async (event) => {
+  await syncSystemMembers()
+})
 
 /**
  * ### exports.deleteMember
@@ -39,8 +51,26 @@ async function deleteDependents(memberId) {
   await Promise.all(promises)
 }
 
+/**
+ * ### deleteImages
+ * Deletes images from Storage that are dependent on the Member
+ * specified in the argument 'memberId'.
+ * @param {string} memberId
+ * @returns
+ */
 async function deleteImages(memberId) {
   const directory = `Images/Members/${memberId}`
   const fileBucket = storage.bucket()
   await fileBucket.deleteFiles({ prefix: directory })
+}
+
+/**
+ * ### syncSystemMembers
+ * Synchronize number of members to the system document.
+ */
+async function syncSystemMembers() {
+  const colRef = firestore.collection('Members')
+  const snapshot = await colRef.get()
+  const members = snapshot.docs.length
+  await firestore.doc('settings/system').update({ members })
 }
